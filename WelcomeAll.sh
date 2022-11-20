ab4d93e23fe6ff441790b8de66dbf75562cb429c
Saigavandi1997 <saigavandi1997@gmail.com>
2022-11-20 15:47:51 +0530

    [Saikanchan]-ReadDictValues

--- WelcomeAll.sh
+++ WelcomeAll.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 echo Welcome Message
+declare -A Results
 read -p "Enter First Number:" a
 read -p "Enter Second Number:" b
 read -p "Enter Third Number:" c
-d=$(($a + $b * $c))
-echo $d
-d1=$(($a * $b + $c))
-echo $d1
-d=$(($c + $a / $b))
-echo $d
-d=$(($a / $b + $c))
-echo $d
+Results=$(($a + $b * $c))
+echo $Results
+Results=$(($a * $b + $c))
+echo $Results
+Results=$(($c + $a / $b))
+echo $Results
+Results=$(($a / $b + $c))
+echo $Results
+#declare -A Results
+echo "a+b*c" ${Results[d1]}
+echo "a*b+c" ${Results[d2]}
+echo "c+a/b" ${Results[d3]}
+echo "a/b+c" ${Results[d4]}
+echo "Total Results " ${#Results[*]}
+Results=d1+d2+d3+d4
+echo ${#Results[Results]}
